ff1cee0e896b6e393cfe95503f8f2cd153a15e4a
Artem Skyba <131376392+Artemskyba@users.noreply.github.com>
2024-01-02 14:36:17 +0200

    Update 9.ts

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -6,8 +6,11 @@ type TestType = {
   title: string;
   likes: number;
   accounts: string[];
-  status: string;
-  details?: object;
+  status: "open" | "close";
+  details?: {
+    createAt: Date,
+    updateAt: Date
+  };
 };
 
 const page1: TestType = {
